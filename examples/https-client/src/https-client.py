9ff2c36631e1c7e051223b60857bd82f6c3f76d1
Simon Deziel <simon.deziel@canonical.com>
2023-09-05 23:26:47 -0400

    https-client: don't use StoredState for client cert storage
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- examples/https-client/src/https-client.py
+++ examples/https-client/src/https-client.py
@@ -18,6 +18,7 @@ from ops.charm import (
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, Application, BlockedStatus, MaintenanceStatus, Unit
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +37,6 @@ class HttpsClientCharm(CharmBase):
 
         # Initialize the persistent storage if needed
         self._stored.set_default(
-            cert="",
             remote_lxd_is_clustered="false",
         )
 
@@ -50,12 +50,49 @@ class HttpsClientCharm(CharmBase):
         self.framework.observe(self.on.https_relation_changed, self._on_https_relation_changed)
         self.framework.observe(self.on.https_relation_created, self._on_https_relation_created)
 
+    def generate_cert(self) -> None:
+        """Generate a self-signed certificate for the client."""
+        cmd = [
+            "openssl",
+            "req",
+            "-x509",
+            "-newkey",
+            "ec",
+            "-pkeyopt",
+            "ec_paramgen_curve:secp384r1",
+            "-sha384",
+            "-keyout",
+            "client.key",
+            "-out",
+            "client.crt",
+            "-nodes",
+            "-subj",
+            f"/CN={self.unit.name.replace('/', '-')}",
+            "-days",
+            "3650",
+        ]
+        try:
+            subprocess.run(cmd, capture_output=True, check=True)
+        except subprocess.CalledProcessError as e:
+            self.unit_blocked(f'Failed to run "{e.cmd}": {e.stderr} ({e.returncode})')
+            return
+
+    @property
+    def cert(self) -> str:
+        """Return the client certificate."""
+        try:
+            cert: str = Path("client.crt").read_text()
+        except FileNotFoundError:
+            return ""
+
+        return cert
+
     def config_to_databag(self) -> dict:
         """Translate config data to be storable in a data bag."""
         # Prepare data to be sent (only strings, no bool nor None)
         d = {
             "version": "1.0",
-            "certificate": self._stored.cert,
+            "certificate": self.cert,
         }
 
         projects: str = self.config.get("projects", "")
@@ -82,42 +119,15 @@ class HttpsClientCharm(CharmBase):
 
     def _on_charm_install(self, event: InstallEvent) -> None:
         """Generate a self-signed cert if needed."""
-        if self._stored.cert != "":
+        if self.cert:
             return
 
         self.unit_maintenance("Generating a self-signed cert")
-        cmd = [
-            "openssl",
-            "req",
-            "-x509",
-            "-newkey",
-            "ec",
-            "-pkeyopt",
-            "ec_paramgen_curve:secp384r1",
-            "-sha384",
-            "-keyout",
-            "client.key",
-            "-out",
-            "client.crt",
-            "-nodes",
-            "-subj",
-            f"/CN={self.unit.name.replace('/', '-')}",
-            "-days",
-            "3650",
-        ]
-        try:
-            subprocess.run(cmd, capture_output=True, check=True)
-        except subprocess.CalledProcessError as e:
-            self.unit_blocked(f'Failed to run "{e.cmd}": {e.stderr} ({e.returncode})')
-            return
-
-        # Save the self-signed certificate for later use
-        with open("client.crt") as f:
-            self._stored.cert = f.read()
+        self.generate_cert()
 
     def _on_charm_start(self, event: StartEvent) -> None:
         """Start the unit if a cert was properly generated on install."""
-        if self._stored.cert == "":
+        if not self.cert:
             self.unit_blocked("no cert available")
             return
 
@@ -223,7 +233,7 @@ class HttpsClientCharm(CharmBase):
 
     def _on_https_relation_created(self, event: RelationCreatedEvent) -> None:
         """Upload our client certificate to the remote unit."""
-        if self._stored.cert == "":
+        if not self.cert:
             logger.error("no cert available")
             return
 
